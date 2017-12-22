6bd9ad0ce77eb9df15c173603c949de5a95b06c5
ShadowlessMCPE <34737004+ShadowlessMCPE@users.noreply.github.com>
2017-12-22 15:13:47 -0600

    Add files via upload

---
+++ src/BetterMining/BetterMining.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: khoan
+ * Date: 4/26/2016
+ * Time: 8:51 AM
+ */
+
+namespace BetterMining;
+
+use pocketmine\entity\Effect;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as Color;
+use pocketmine\plugin\PluginBase;
+
+class BetterMining extends PluginBase{
+    public function onLoad()
+    {
+        $this->getLogger()->info(Color::DARK_RED . "The plugin is ready to go!");
+    }
+    
+    public function onEnable()
+    {
+        $this->getLogger()->info(Color::GOLD . "BetterMining enabled! Made by KhoaHoang!");
+        $this->getLogger()->info(Color::GREEN . "This plugin is very useful for mining servers! :D");
+    }
+    
+    public function onJoin(PlayerJoinEvent $event){
+        $player = $event->getPlayer();
+        $name = $player->getPlayer();
+        if($player instanceof Player){
+            $player->addEffect(Effect::getEffect(3));
+            $player->addEffect(Effect::getEffect(16));
+            $player->addEffect(Effect::getEffect(23));
+            $player->getEffect(3)->setAmplifier(5);
+            $player->getEffect(16)->setAmplifier(5);
+            $player->getEffect(23)->setAmplifier(10);
+            $player->getEffect(3)->setDuration(20000);
+            $player->getEffect(16)->setDuration(20000);
+            $player->getEffect(23)->setDuration(20000);
+            $player->sendMessage(Color::DARK_PURPLE . "Hey " . $name . ", you received some effects for mining easier! :D");
+        }
+    }
+}
